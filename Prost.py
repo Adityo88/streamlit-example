e6bb5145203948edcc83875e57896859db488358
Adityo88 <114216590+Adityo88@users.noreply.github.com>
2023-03-09 18:12:03 +0700

    Update Prost.py

--- Prost.py
+++ Prost.py
@@ -3,6 +3,32 @@ import pandas as pd
 
 def main():
     st.title("Streamlit Forms & Salary Calculator")
+    menu = ["Home" , "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
+    
+if choice == "Home":
+    st.subheader("Forms Tutorial") 
+    #method1
+    with st.form(key='form1'):
+        firstname = st.text_input("Firstname")
+        lastname = st.text_input("Lastname")
+        dob = st.date_input("Date of Birth)
+        
+        submit_button = st.form_submit_button(label='SignUp')
+                 
+    if submit_button:
+       st.succsess("Hello {} you ve created an account". format(firstname))
+    #method2
+    form2 = st.form(key='form2)
+    username = form2.text_input("Username")
+    jobtype = form2.selectbox("Job,["Dev","Data Scientist", "Data Analyst"])
+    submit_button2 = form2.form_submit_button("Login")
+                            
+                            
+else:
+    st.subheader("About")
+    
+    
     
 if __name__ == '__main__':
      main()
